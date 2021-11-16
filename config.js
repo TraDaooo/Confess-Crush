7d7aa8a5ad8f112e409dd9fb5ebe54723e47e3e0
TraDaooo <94387708+TraDaooo@users.noreply.github.com>
2021-11-17 01:22:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
     titleWeb: "Gửi cho ... ❤",
     introTitle: 'Hello Ngân Lee',
-    introDesc: `Mới đó đã nửa năm quen nhau rồi nhỉ, nhớ hồi chị mới vào room vẫn còn acc ngựa, em mới lv6. Duyên số thế nào lại gặp được em, lỡ va phải ánh mắt em, rùi mê em từ đó đến giờ🤣`,
+    introDesc: 'Mới đó đã nửa năm quen nhau rồi nhỉ, nhớ hồi chị mới vào room vẫn còn acc ngựa, em mới lv6. Duyên số thế nào lại gặp được em, lỡ va phải ánh mắt em, rùi mê em từ đó đến giờ🤣',
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰Phải chăng em đã say ngay từ lúc thấy nụ cười ấy',
     desc: 'DO YOU LOVE ME ',
@@ -14,5 +14,4 @@ const CONFIG = {
     messDesc: 'hmmm,Dù có chuyện gì đi nữa, chị vẫn ở đây với em. Yêu em .',
     btnAccept: 'Okiiiii lun <3',
     messLink:'https://www.youtube.com/watch?v=2MZ_oQOGC24'
-}
 }
