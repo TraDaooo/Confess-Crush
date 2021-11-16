c324d17bcc0ee6029686059b2b7862b996038e22
TraDaooo <94387708+TraDaooo@users.noreply.github.com>
2021-11-17 01:51:04 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,18 @@
 const CONFIG = {
     titleWeb: "Gửi cho ... ❤",
     introTitle: 'Hello Ngân Lee',
-    introDesc: 'Mới đó đã nửa năm quen nhau rồi nhỉ, nhớ hồi chị mới vào room vẫn còn acc ngựa, em mới lv6. Duyên số thế nào lại gặp được em, lỡ va phải ánh mắt em, rùi mê em từ đó đến giờ🤣🤣.    btnIntro: '^^Yêu^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    desc: 'DO YOU LOVE ME',
+    introDesc: `Mới đó đã nửa năm quen nhau rồi nhỉ, nhớ hồi chị mới vào room vẫn còn acc ngựa, em mới lv6. Duyên số thế nào lại gặp được em, lỡ va phải ánh mắt em, rùi mê em từ đó đến giờ🤣`,
+    btnIntro: '^^HiHi^^',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰Phải chăng em đã say ngay từ lúc thấy nụ cười ấy',
+    desc: 'DO YOU LOVE ME ',
     btnYes: 'YESSSS <33',
     btnNo: 'Bấm No đi :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao chúng ta lại gặp nhau ❤',
-    btnReply: 'hihi',
+    btnReply: 'hihi <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Chị biết mà 🥰. Cho Hưng Lee ra rìa 😘😘',
     messDesc: 'hmmm,Dù có chuyện gì đi nữa, chị vẫn ở đây với em. Yêu em .',
-    btnAccept: 'Okiiiii lun ❤',
-    messLink: 'https://www.youtube.com/watch?v=2MZ_oQOGC24'
+    btnAccept: 'Okiiiii lun <3',
+    messLink:'https://www.youtube.com/watch?v=2MZ_oQOGC24'
+}
 }
