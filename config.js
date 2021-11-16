ee7cbf75e4894489ca382ae8652058496fa37654
TraDaooo <94387708+TraDaooo@users.noreply.github.com>
2021-11-16 22:48:27 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,16 @@
 const CONFIG = {
-    titleWeb: "Gửi cho ... <3",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Cả cuộc đời em chưa bao giờ viết nhiều code như thế này
-Sách ở trường em đọc ko hiểu, mà tự viết thì chỗ thừa chỗ thiếu
-Em vốn ko định nhờ anh dạy đâu :))) nhưng ko dạy em thực sự ko biết làmmmm
-Thật ra em nhờ bạn làm hộ cũng được ấy, mà thế có vẻ không thật tâm cho lắm. Cho nên dù hơi phèn ỉa một tí….. nhưng bạn đừng chê nhá!!!`,
-    btnIntro: '^^Yêu em^^',
+    titleWeb: "Gửi cho ... ❤",
+    introTitle: 'Hello Ngân Lee',
+    introDesc: Mới đó đã nửa năm quen nhau rồi nhỉ, nhớ hồi chị mới vào room vẫn còn acc ngựa, em mới lv6. Duyên số thế nào lại gặp được em, lỡ va phải ánh mắt em, rùi mê em từ đó đến giờ🤣🤣.    btnIntro: '^^Yêu^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     desc: 'DO YOU LOVE ME',
     btnYes: 'YESSSS <33',
     btnNo: 'Bấm No đi :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao chúng ta lại gặp nhau ❤',
     btnReply: 'hihi',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'hmmm,Dù có chuyện gì đi nữa, mình vẫn ở đây với bạn. Ko biết tương lai ntn, chỉ mong ko sai người.',
-    btnAccept: 'Okiiiii lun <3',
+    mess: 'Chị biết mà 🥰. Cho Hưng Lee ra rìa 😘😘',
+    messDesc: 'hmmm,Dù có chuyện gì đi nữa, chị vẫn ở đây với em. Yêu em .',
+    btnAccept: 'Okiiiii lun ❤',
     messLink: 'https://www.youtube.com/watch?v=2MZ_oQOGC24'
 }
