d7140da9c13f9c20490ef04e07ac87d413683ae5
TraDaooo <94387708+TraDaooo@users.noreply.github.com>
2024-03-24 20:01:43 +0900

    Update config.js

--- config.js
+++ config.js
@@ -2,16 +2,16 @@ const CONFIG = {
     titleWeb: "Send to ... ❤",
     introTitle: 'Hello Soda',
     introDesc: 'To the world you may be one person, but to one person you may be the world',
-    btnIntro: '^^HiHi^^',
+    btnIntro: '^^Okey^^',
     title: 'It only takes a second to say I love you, but it will take a lifetime to show you how much',
     desc: 'DO YOU LOVE ME ',
     btnYes: 'YESSSS <33',
-    btnNo: 'Choose NO :3',
+    btnNo: 'Please Choose NOOO :3',
     question: 'There is only one happiness in this life, to love and be loved',
-    btnReply: 'hihi <3',
+    btnReply: 'Love you<3',
     reply: 'Some people are worth melting for<33333333',
     mess: 'I know it😘😘',
     messDesc: 'I love you not because of who you are, but because of who I am when I am with you .',
     btnAccept: 'Okey <3',
-    messLink:'https://www.youtube.com/watch?v=2MZ_oQOGC24'
+    messLink:'https://youtu.be/8xg3vE8Ie_E?si=m6WoagcGkB8U0esi'
 }
